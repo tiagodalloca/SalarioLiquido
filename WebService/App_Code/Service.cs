fae189c877e8950f575e19f6a1dea0c796c8ff99
MrDallOca <tdalloca@gmail.com>
2017-02-16 12:14:17 -0200

    isCpfValido feito

--- WebService/App_Code/Service.cs
+++ WebService/App_Code/Service.cs
@@ -11,15 +11,47 @@ using System.Web.Services;
 
 public class Service : System.Web.Services.WebService
 {
-    public Service() {
+    public Service()
+    {
 
         //Uncomment the following line if using designed components 
         //InitializeComponent(); 
     }
 
     [WebMethod]
-    public bool isCPFValido(string cpf){
-        return true;
+    public bool isCPFValido(string cpf)
+    {
+        //extrai os dígitos do CPF
+        byte num1 = (byte)char.GetNumericValue(cpf[0]);
+        byte num2 = (byte)char.GetNumericValue(cpf[1]);
+        byte num3 = (byte)char.GetNumericValue(cpf[2]);
+        byte num4 = (byte)char.GetNumericValue(cpf[3]);
+        byte num5 = (byte)char.GetNumericValue(cpf[4]);
+        byte num6 = (byte)char.GetNumericValue(cpf[5]);
+        byte num7 = (byte)char.GetNumericValue(cpf[6]);
+        byte num8 = (byte)char.GetNumericValue(cpf[7]);
+        byte num9 = (byte)char.GetNumericValue(cpf[8]);
+        byte num10 = (byte)char.GetNumericValue(cpf[9]);
+        byte num11 = (byte)char.GetNumericValue(cpf[10]);
+
+        if ((num1 == num2) && (num2 == num3) && (num3 == num4) && (num4 == num5) && (num5 == num6) && (num6 == num7) && (num7 == num8) && (num8 == num9) && (num9 == num10) && (num10 == num11))
+            return false;
+        else
+        {
+            var soma1 = num1 * 10 + num2 * 9 + num3 * 8 + num4 * 7 + num5 * 6 + num6 * 5 + num7 * 4 + num8 * 3 + num9 * 2;
+            var resto1 = (soma1 * 10) % 11;
+
+            if (resto1 == 10)
+                resto1 = 0;
+
+            var soma2 = num1 * 11 + num2 * 10 + num3 * 9 + num4 * 8 + num5 * 7 + num6 * 6 + num7 * 5 + num8 * 4 + num9 * 3 + num10 * 2;
+            var resto2 = (soma2 * 10) % 11;
+
+            if (resto2 == 10)
+                resto2 = 0;
+
+            return ((resto1 == num10) && (resto2 == num11));
+        }
     }
 
 }
